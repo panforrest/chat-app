756fbc192724f01f4520bb50d4313feea7b72a79
panforrest <guoqianp@gmail.com>
2018-03-14 00:22:37 -0400

    1:02:10 my attempt for get handler to list all records of the resource

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,26 @@ const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
 
+//valid resources
+const resources = [   //const resource = {
+	'room',
+	'topic',
+	'reply',
+	'user'
+]
+
 router.post('/:resource', (req, res) => {
-    const params = req.body
-    turbo.create('room', params)
+    const resource = req.params.resource
+
+    if (resources.indexOf(resource) == -1){ //invalid resource
+    	res.json({
+    		confirmation: 'fail',
+    		message: 'invalid source: ' + resource
+    	})
+        return
+    }
+
+    turbo.create(resource, req.body)
 	.then(data => {
 	    res.json({
 	    	confirmation: 'success',
@@ -18,12 +35,32 @@ router.post('/:resource', (req, res) => {
 			message: err.message
 		})
 	})
-	
+})
 
-    // res.json({
-    // 	confirmation: 'success' ,
-    // 	data: req.body
-    // })
+router.get('/:resource', (req, res) => {
+    const resource = req.params.resource
+
+    if (resources.indexOf(resource) == -1){ //invalid resource
+    	res.json({
+    		confirmation: 'fail',
+    		message: 'invalid source: ' + resource
+    	})
+        return
+    }
+
+    turbo.fetch(resource, req.query)  //turbo.fetch(resource, null)
+    .then(data => {
+        res.json({
+        	confirmation: 'success',
+        	data: data
+        })
+    })
+    .catch(err => {
+        res.json({
+        	confirmation: 'fail',
+        	message: err.message
+        })
+    })
 })
 
 module.exports = router
